471c3801d810db350efcaaba34091d6dce4d9501
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2021-07-25 21:17:43 +0300

    Add files via upload

---
+++ repeating_rows.py
@@ -0,0 +1,12 @@
+
+### 3 digit ile baslayalim:1,2,3. Sirayi takip eden satir kendinden onceki 3 satirin birlesiminden olusuyor. Bu kurala gore 100.satirdaki digitlerin toplamini bulan kod asagidaki gibidir###
+### Her bir satirdaki digitlerin toplamlarini tutarak gidersek daha az yer kullanmis oluruz ve daha hizli sonuc aliriz:###
+
+x,y,z = 1,2,3
+row = 98 #hesaplanmasini istedigimiz satir(ilk 3 satir 1,2,3'tur. 100.satir icin 100-3=97 kez asagidaki dongu hesaplanmalidir.)
+
+
+for i in range(row):
+    x,y,z=y,z,x+y+z#bir sonraki satirdaki digitlerin toplamini hesaplayan satir
+
+print(z)
